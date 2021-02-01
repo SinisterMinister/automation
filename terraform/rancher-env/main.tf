f3a39cc49d8798782b0f706a648e2acc7862c2f2
Codey <codey.whitt@gmail.com>
2021-01-31 19:18:21 -0700

    fix ref

--- terraform/rancher-env/main.tf
+++ terraform/rancher-env/main.tf
@@ -20,8 +20,8 @@ locals {
   working_directory = var.working_directory
   manifest = yamldecode(file("${local.working_directory}/manifest.yaml"))
 
-  catalogs = local.manifest.live ? {for catalog in manifest.catalogs : catalog.name => catalog} : {}
-  apps = local.manifest.live ? {for app in manifest.charts : app.name => app} : {}
+  catalogs = local.manifest.live ? {for catalog in local.manifest.catalogs : catalog.name => catalog} : {}
+  apps = local.manifest.live ? {for app in local.manifest.charts : app.name => app} : {}
 }
 
 data sops_file secrets {
