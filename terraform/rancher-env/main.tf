180b438f3ee91e2c22f58ebe18b2086b586d80c1
Codey <codey.whitt@gmail.com>
2021-01-31 17:17:36 -0700

    add rancher-env module

---
+++ terraform/rancher-env/main.tf
@@ -0,0 +1,55 @@
+provider "sops" {}
+
+# provider "docker" {
+#   registry_auth {
+#     address = "hub.sinimini.com"
+#     username = data.sops_file.secrets.data["jcr.user"]
+#     password = data.sops_file.secrets.data["jcr.password"]
+#   }
+# }
+
+provider rancher2 {
+    api_url = "https://rancher.sinimini.com"
+    access_key = data.sops_file.secrets.data["rancher.accessKey"]
+    secret_key = data.sops_file.secrets.data["rancher.secretKey"]
+}
+
+locals {
+  sops_file_path = var.sops_file_path
+  name = var.environment
+  working_directory = var.working_directory
+  manifest = yamldecode(file(local.working_directory+"/manifest.yaml"))
+
+  catalogs = {for catalog in manifest.catalogs : catalog.name => catalog}
+  apps = {for app in manifest.charts : app.name => app}
+}
+
+data sops_file secrets {
+  source_file = local.sops_file_path
+}
+
+# data docker_registry_image moneytree {
+#     name = "hub.sinimini.com/docker/moneytree:latest"
+# }
+
+
+resource rancher2_project project {
+  cluster_id = "local"
+  name = local.name
+}
+
+resource rancher2_namespace namespace {
+    project_id = rancher2_project.project.id
+    name = local.name
+}
+
+resource rancher2_app moneytree {
+  for_each = apps
+  catalog_name = each.value.catalog
+  name = each.value.displayName
+  project_id = rancher2_project.project.id
+  target_namespace = rancher2_namespace.namespace.name
+  template_name = each.value.name
+  template_version = each.value.version
+  values_yaml = base64encode(yamlencode(each.value.answers))
+}
---
+++ terraform/rancher-env/vars.tf
@@ -0,0 +1,11 @@
+variable sops_file_path {
+    type = string
+}
+
+variable environment {
+    type = string
+}
+
+variable working_directory {
+    type = string
+}
